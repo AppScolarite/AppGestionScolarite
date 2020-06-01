5b8bcf8ccafa23a13bd5b6164f0944df7e876022
Hicham <tony.theedev@gmail.com>
2020-06-01 14:02:19 +0100

    mise a jour script (insert)

--- DataBase/Gestion_Scolarite.sql
+++ DataBase/Gestion_Scolarite.sql
@@ -134,56 +134,100 @@ create TRIGGER ChECK_ETD
   END
 GO
 
-insert into PERSONNEL (nom_personnel, prenom_personnel, date_naissance_personnel,email_personnel,telephone_personnel, sexe, adresse , username, mot_de_passe)
-values
-('Hicham','Oussama', '01/01/1997','hichal@gmail.com','0612345768','Homme','adresse1','hicham','hicham123')
-
-insert into BRANCHE ( libelle_branche,prerequis_note,description_branche)
-values 
-('branche 1',15,'Descprtion 1')
-
-insert into NIVEAU (libelle_niveau,description_niveau)
-values 
-('Niveau 1','Description1')
-
-insert into GROUPE(branche#, niveau#,libelle_grp)
-values
-(1,1,'Groupe 1'),
-(1,1,'Groupe 2'),
-(1,1,'Groupe 3'),
-(1,1,'Groupe 4')
-
-insert into ETUDIANT (code_massar,nom,prenom ,date_naissance, date_inscription , email , telephone , a_deja_redouble , sexe , adresse ,groupe# ,username ,mot_de_passe )
-values 
-('H1','Yaagoubi','Noureddine','16/01/1998','20/09/2019','noureddine@gmail.com','0642833827',0,'Homme','adresse1',1,'noureddine','noureddine123'),
-('H2','Saffih','Hicham','25/08/1997','20/09/2019','hicham@gmail.com','0666201740',0,'Homme','adresse2',1,'tony','tony123')
-
-insert into PROFESSEUR 
-values
-('P1','F123','Ziane','Mohammed','01/01/1995','28/02/2020','CDI','ziane@gmail.com','0606060606','Homme','adresse1','Celibataire','ziane','ziane123')
-
-insert into MATIERE(LBL_Matiere,Date_Ajout,Coeff)
-values
-('Français','28/05/2020',5),
-('Asp.Net','28/05/2020',5)
-
-insert into NOTE (matiere#, Valeur_Note,etudiant_)
-values 
-(1,15.5,'h1'),
-(1,9,'h1'),
-(1,17.5,'h1'),
-(2,17.5,'h1'),
-
-(1,13,'h2'),
-(1,7.5,'h2'),
-(1,18,'h2'),
-(2,15,'h2')
-
-
-insert into ENSEIGNEMENT
-values
-('P1',1,1)
-
+SET IDENTITY_INSERT [dbo].[PERSONNEL] ON 
+GO
+INSERT [dbo].[PERSONNEL] ([id_personnel], [nom_personnel], [prenom_personnel], [date_naissance_personnel], [photo_personnel], [email_personnel], [telephone_personnel], [sexe], [adresse], [username], [mot_de_passe]) VALUES (1, N'Hicham', N'Oussama', CAST(N'1997-01-01' AS Date), NULL, N'hichal@gmail.com', N'0612345768', N'Homme', N'adresse1', N'hicham', N'hicham123')
+GO
+SET IDENTITY_INSERT [dbo].[PERSONNEL] OFF
+GO
+SET IDENTITY_INSERT [dbo].[BRANCHE] ON 
+GO
+INSERT [dbo].[BRANCHE] ([id_branche], [libelle_branche], [prerequis_note], [description_branche]) VALUES (1, N'Isi', 13, N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+INSERT [dbo].[BRANCHE] ([id_branche], [libelle_branche], [prerequis_note], [description_branche]) VALUES (2, N'Com', 11, N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+INSERT [dbo].[BRANCHE] ([id_branche], [libelle_branche], [prerequis_note], [description_branche]) VALUES (3, N'Mtd', 10, N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+SET IDENTITY_INSERT [dbo].[BRANCHE] OFF
+GO
+SET IDENTITY_INSERT [dbo].[NIVEAU] ON 
+GO
+INSERT [dbo].[NIVEAU] ([id_niveau], [libelle_niveau], [description_niveau]) VALUES (1, N'Niveau 1', N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+INSERT [dbo].[NIVEAU] ([id_niveau], [libelle_niveau], [description_niveau]) VALUES (2, N'Niveau 2', N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+INSERT [dbo].[NIVEAU] ([id_niveau], [libelle_niveau], [description_niveau]) VALUES (3, N'Niveau 3', N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+INSERT [dbo].[NIVEAU] ([id_niveau], [libelle_niveau], [description_niveau]) VALUES (4, N'Niveau 4', N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+INSERT [dbo].[NIVEAU] ([id_niveau], [libelle_niveau], [description_niveau]) VALUES (5, N'Niveau 5', N'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro iste earum, sequi aliquid aut cumque voluptatem perspiciatis qui debitis.')
+GO
+SET IDENTITY_INSERT [dbo].[NIVEAU] OFF
+GO
+SET IDENTITY_INSERT [dbo].[GROUPE] ON 
+GO
+INSERT [dbo].[GROUPE] ([id_groupe], [branche#], [niveau#], [libelle_grp]) VALUES (1, 1, 3, N'isi 3')
+GO
+INSERT [dbo].[GROUPE] ([id_groupe], [branche#], [niveau#], [libelle_grp]) VALUES (2, 1, 4, N'isi 4')
+GO
+INSERT [dbo].[GROUPE] ([id_groupe], [branche#], [niveau#], [libelle_grp]) VALUES (3, 2, 1, N'com 1')
+GO
+INSERT [dbo].[GROUPE] ([id_groupe], [branche#], [niveau#], [libelle_grp]) VALUES (4, 3, 4, N'mtd 4')
+GO
+SET IDENTITY_INSERT [dbo].[GROUPE] OFF
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H1258', N'Ahmed ', N'tizniti', CAST(N'1997-07-29' AS Date), CAST(N'2020-06-01' AS Date), N'Ahmed@gmail.com', N'0662149873', 1, N'Homme', N'adresse4', 1, N'Ahmed', N'moumou123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H1697', N'Mustafa', N'nourawi', CAST(N'1997-02-26' AS Date), CAST(N'2020-06-01' AS Date), N'Mustafa@gmail.com', N'0763851924', 0, N'Homme', N'adresse7', 1, N'Mustafa', N'Mustafa123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H1897', N'Yaagoubi', N'Noureddine', CAST(N'1998-01-16' AS Date), CAST(N'2019-09-20' AS Date), N'noureddine@gmail.com', N'0642833827', 0, N'Homme', N'adresse1', 1, N'noureddine', N'noureddine123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H2578', N'Saffihh', N'Hichamm', CAST(N'1997-08-25' AS Date), CAST(N'2019-09-20' AS Date), N'hicham@gmail.com', N'0666201747', 0, N'Homme', N'adresse2', 1, N'tony', N'tony123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H3987', N'Essirioui', N'Amina', CAST(N'1997-02-26' AS Date), CAST(N'2020-06-01' AS Date), N'Amina@gmail.com', N'0662148753', 0, N'Femme', N'adresse3', 1, N'moumou', N'moumou123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H4877', N'Nisrine', N'Hadiwi', CAST(N'1997-08-25' AS Date), CAST(N'2020-06-01' AS Date), N'Nisrine@gmail.com', N'0684793215', 0, N'Femme', N'adresse5', 1, N'Nisrine', N'Nisrine123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H55439', N'abdlawi', N'Adam', CAST(N'1995-05-02' AS Date), CAST(N'2020-06-01' AS Date), N'Adam@gmail.com', N'0598721674', 0, N'Homme', N'adresse9', 1, N'Adam', N'Adam123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H6746', N'Israe', N'Berber', CAST(N'2001-06-05' AS Date), CAST(N'2020-06-01' AS Date), N'israe@gmail.com', N'0236789415', 0, N'Femme', N'adresse 10', 2, N'israe', N'israe123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H9173', N'Aamari', N'Roqaya', CAST(N'1995-02-15' AS Date), CAST(N'2020-06-01' AS Date), N'Roqaya@gmail.com', N'0874923641', 0, N'Femme', N'adresse8', 1, N'Roqaya', N'Roqaya123')
+GO
+INSERT [dbo].[ETUDIANT] ([code_massar], [nom], [prenom], [date_naissance], [date_inscription], [email], [telephone], [a_deja_redouble], [sexe], [adresse], [groupe#], [username], [mot_de_passe]) VALUES (N'H9821', N'Nihal', N'Bkkay', CAST(N'1996-06-09' AS Date), CAST(N'2020-06-01' AS Date), N'Nihal@gmail.com', N'0598721634', 1, N'Femme', N'adresse6', 1, N'Nihal', N'Nihal123')
+GO
+INSERT [dbo].[PROFESSEUR] ([Code_Pro_Nationnal], [Cin], [Nom], [Prenom], [Date_Naissance], [Date_Commencement_Contrat], [Type_Contrat], [Email], [Telephone], [sexe], [Adresse], [Situation_Familliale], [username], [mot_de_passe]) VALUES (N'P1', N'F123', N'Ziane', N'Mohammed', CAST(N'1995-01-01' AS Date), CAST(N'2020-02-28' AS Date), N'CDI', N'ziane@gmail.com', N'0606060606', N'Homme', N'adresse1', N'Celibataire', N'ziane', N'ziane123')
+GO
+SET IDENTITY_INSERT [dbo].[MATIERE] ON 
+GO
+INSERT [dbo].[MATIERE] ([id_matiere], [LBL_Matiere], [Date_Ajout], [Coeff]) VALUES (1, N'Français', CAST(N'2020-05-28' AS Date), 5)
+GO
+INSERT [dbo].[MATIERE] ([id_matiere], [LBL_Matiere], [Date_Ajout], [Coeff]) VALUES (2, N'Asp.Net', CAST(N'2020-05-28' AS Date), 5)
+GO
+SET IDENTITY_INSERT [dbo].[MATIERE] OFF
+GO
+INSERT [dbo].[ENSEIGNEMENT] ([professeur#], [groupe#], [matiere#]) VALUES (N'P1', 1, 1)
+GO
+SET IDENTITY_INSERT [dbo].[NOTE] ON 
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (1, 1, 15.5, N'h1')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (2, 1, 9, N'h1')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (3, 1, 17.5, N'h1')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (4, 2, 17.5, N'h1')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (5, 1, 13, N'h2')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (6, 1, 7.5, N'h2')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (7, 1, 18, N'h2')
+GO
+INSERT [dbo].[NOTE] ([id_note], [matiere#], [Valeur_Note], [etudiant_]) VALUES (8, 2, 15, N'h2')
+GO
+SET IDENTITY_INSERT [dbo].[NOTE] OFF
+GO
 
 --utilitaire---------------------
 --DBCC CHECKIDENT ('NOTE', RESEED, 0) -- redefinir IDENITY à 0
