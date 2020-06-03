12385628c02209467ddd4e99a5f85f1e945e036c
YNounou <50302074+YNounou@users.noreply.github.com>
2020-06-03 19:26:32 +0100

    noureddine

--- src/Application/Controllers/LandingController.java
+++ src/Application/Controllers/LandingController.java
@@ -1,4 +1,36 @@
 package Application.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.net.URL;
+
 public class LandingController {
+    
+    @FXML
+    private Button startlogin;
+
+    public void goTologin() throws Exception{
+        URL url = new File("src/Application/Views/Login.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        Stage stageCurrect = (Stage) startlogin.getScene().getWindow();
+        stageCurrect.close();
+        stage.show();
+    }
+
+    @FXML
+    public void btnClose_Click(){
+        Stage stage = (Stage) startlogin.getScene().getWindow();
+        stage.close();
+    }
 }
